8f4426647a87f2fb17ad73a6e1ebe3ee2b0bcbe5
shreydan <iamshreydan@gmail.com>
2021-11-26 19:20:38 +0530

    authentication based redirecting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,15 @@
-import { useWeb3React } from "@web3-react/core"
-import { useHistory } from "react-router";
 import { useEffect, useState } from "react";
-import { injected } from "../Components/Wallet/Connectors"
+import { useHistory } from "react-router";
 
 function Home() {
 
-    const history = useHistory()
-    const [connected, setConnected] = useState(false)
-    const { active, account, library, connector, activate, deactivate } = useWeb3React()
-
-
-    async function connect() {
-        try {
-            await activate(injected);
-            alert("Connnected to your Metamask")
-            setConnected(true)
-            history.push('/vote')
-        } catch (error) {
-            console.error(error)
-            alert("Failed connect")
-        }
-    }
-
-    useEffect(() => {
-        connect();
-    },[])
+    const history = useHistory();
 
     return (
         <div className="App">
-            Connecting to Metamask
+            campus vote
+
+            <button onClick={() => {history.push('/login')}}>LOGIN</button>
         </div>
     );
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useWeb3React } from "@web3-react/core";
+import { useHistory } from "react-router";
+import { useEffect, useContext } from "react";
+import { injected } from "../Components/Wallet/Connectors";
+import { AuthContext } from "../Contexts/AuthContext";
 
 function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const history = useHistory();
+
+  const { login, wallet } = useContext(AuthContext);
+  const { loggedIn } = login;
+  const { walletConnected, setWalletConnected } = wallet;
+
+  console.log(loggedIn, walletConnected);
+
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+
+  const connect = async () => {
+        
+    await activate(injected)
+        .then(() => {
+            setWalletConnected(true)
+            alert("wallet connected")
+        })
+        .catch((err) => {
+            console.log(err)
+            alert("Check console for error")
+        })
+
+  }
+
+  const redirectToVote = (e) => {
+    e.preventDefault();
+    if(loggedIn && walletConnected) {
+        history.push('/vote')
+    }
+  }
+
+
+  useEffect(() => {
+    connect();
+  },[]);
+
+  return (
+    <div className="App">
+      {loggedIn && walletConnected ? (
+        <p>Redirecting...</p>
+      ) : (
+        <p>Connecting...</p>
+      )}
+
+      <button onClick={redirectToVote}>VOTE</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
