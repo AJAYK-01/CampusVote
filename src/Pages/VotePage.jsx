81eed04059391b299d2462b44af7cc38f2847008
shreydan <iamshreydan@gmail.com>
2021-11-27 23:15:16 +0530

    votepage changes

--- src/Pages/VotePage.jsx
+++ src/Pages/VotePage.jsx
@@ -3,26 +3,23 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-import VoteService from '../Helpers/voter'
+import VoteService from "../Helpers/voter";
 
 function VotePage() {
-
-
+    
   const voter = new VoteService();
 
-
   const [tokenReceived, setTokenReceived] = useState(false);
-  async function receiveToken() {
-    // function to receive tokens
-    const count = await voter.getCandidateCount();
-    const candidates = await voter.fetchCandidates()
 
-    setOptions(candidates)
-    setTokenReceived(true)
-}
+
+  async function receiveToken() {
+    const candidates = await voter.fetchCandidates();
+    getCount();
+    setOptions(candidates);
+    setTokenReceived(true);
+  }
 
   const [question, setQuestion] = useState("sample question");
-  
 
   const [selectedOption, setSelectedOption] = useState(null);
   function sendVote(event) {
@@ -34,18 +31,16 @@ function VotePage() {
   }
 
   const [options, setOptions] = useState([
-    'option 1',
-    'option 2',
-    'option 3',
-    'option 4'
+    "option 1",
+    "option 2",
+    "option 3",
+    "option 4",
   ]);
 
-  const selectionOptions = ['Select your option', ...options];
+  const selectionOptions = ["Select your option", ...options];
 
-  
-
-
-  
+  useEffect(() => {
+  }, []);
 
   return (
     <Container
@@ -56,31 +51,32 @@ function VotePage() {
       <h1 className="position-absolute bottom-0">Campus Vote</h1>
       <h1 className="m-5">{question}</h1>
 
-      {tokenReceived && <Form className="w-25">
-        <Form.Group>
-          <FloatingLabel controlId="floatingSelect" label="time to vote">
-            <Form.Select
-              aria-label="voting options"
-              onChange={(event) => setSelectedOption(event.target.value)}
-            >
-              {selectionOptions.map((value, index) => (
-                <option key={index}>{value}</option>
-              ))}
-            </Form.Select>
-          </FloatingLabel>
+      {tokenReceived && (
+        <Form className="w-25">
+          <Form.Group>
+            <FloatingLabel controlId="floatingSelect" label="time to vote">
+              <Form.Select
+                aria-label="voting options"
+                onChange={(event) => setSelectedOption(event.target.value)}
+              >
+                {selectionOptions.map((value, index) => (
+                  <option key={index}>{value}</option>
+                ))}
+              </Form.Select>
+            </FloatingLabel>
 
-          <Button type="button" onClick={sendVote} className="w-100 my-5">
-            SEND YOUR VOTE
-          </Button>
-        </Form.Group>
-      </Form>}
+            <Button type="button" onClick={sendVote} className="w-100 my-5">
+              SEND YOUR VOTE
+            </Button>
+          </Form.Group>
+        </Form>
+      )}
 
-      {
-          !tokenReceived &&
-          <Button type="button" onClick={receiveToken} className="w-25 my-5">
+      {!tokenReceived && (
+        <Button type="button" onClick={receiveToken} className="w-25 my-5">
           Receive token to vote
         </Button>
-      }
+      )}
     </Container>
   );
 }
